d32cc5fed04f618db2b6811429ff5853d70e1ee3
RonGissin <45989167+RonGissin@users.noreply.github.com>
2021-06-19 19:55:46 +0300

    imds

--- cache-server/imds-client.ts
+++ cache-server/imds-client.ts
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch');
+const metadata = require('node-ec2-metadata');
 
 export class IMDSClient {
-    private ec2: any;
-    private getEc2Promisified: any;
     /**
      * @constructor
      */
@@ -12,14 +11,22 @@ export class IMDSClient {
         let ip;
 
         try {
-            const res = await fetch("http://169.254.169.254/latest/meta-data/public-ipv4");
-            console.log("response ==== " + JSON.stringify(res));
-            ip = await res.text();
-        } catch(e) {
+            ip = await metadata.getMetadataForInstance('public-ipv4');
+        } catch (e) {
             console.log(e);
-            return "0";
         }
 
         return ip;
+
+        // try {
+        //     const res = await fetch("http://169.254.169.254/latest/meta-data/public-ipv4");
+        //     console.log("response ==== " + JSON.stringify(res));
+        //     ip = await res.text();
+        // } catch(e) {
+        //     console.log(e);
+        //     return "0";
+        // }
+
+        // return ip;
     }
 }
