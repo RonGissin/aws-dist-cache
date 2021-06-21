c178ad13b423bd2e63b276e1ddde98b36e9ff2a3
Ron Gissin <t-rogiss@microsoft.com>
2021-06-21 19:47:10 +0300

    asdfasdf

--- cache-manager/app.ts
+++ cache-manager/app.ts
@@ -142,7 +142,7 @@ app.get("/:key", async (req, res) => {
 
 async function tryGetSmallestPoolIdAsync(): Promise<Maybe<string>> {
     let smallestPoolId: Maybe<string> = Nothing();
-    let smallestPoolSize: number;
+    let smallestPoolSize: number = 0;
     let poolSize: number;
 
     primaryToNodeListMap.forEach(async (nodes: string[], key: string) => {
